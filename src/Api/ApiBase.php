248f1bc15a24143b002383a1d81beda582896c05
Craymend <craymend1@gmail.com>
2021-11-16 14:59:39 -0800

    add curly braces to uuid

--- src/Api/ApiBase.php
+++ src/Api/ApiBase.php
@@ -36,19 +36,13 @@ class ApiBase
             $charid = strtoupper(md5(uniqid(rand(), true)));
             $hyphen = chr(45);// "-"
 
-            // $uuid = chr(123)// "{"
-            //     .substr($charid, 0, 8).$hyphen
-            //     .substr($charid, 8, 4).$hyphen
-            //     .substr($charid,12, 4).$hyphen
-            //     .substr($charid,16, 4).$hyphen
-            //     .substr($charid,20,12)
-            //     .chr(125);// "}"
-
-            $uuid = substr($charid, 0, 8).$hyphen
+            $uuid = chr(123)// "{"
+                .substr($charid, 0, 8).$hyphen
                 .substr($charid, 8, 4).$hyphen
                 .substr($charid,12, 4).$hyphen
                 .substr($charid,16, 4).$hyphen
-                .substr($charid,20,12);
+                .substr($charid,20,12)
+                .chr(125);// "}"
 
             return $uuid;
         }
